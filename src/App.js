5d20acc973b1e58c34dbd056ee7a010e58b02bac
Lhazael <lisa.hazael@gmail.com>
2021-05-27 10:02:02 +0200

    Iteration 0-4 done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
+import FoodBox from './Components/FoodBox';
+import FoodForm from './Components/FoodForm';
+import Search from './Components/Search';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  state = {
+    foods: foods,
+    searchValue: "",
+};
+
+handleForm = () => {
+  const foodForm = document.getElementById("foodForm");
+  foodForm.classList.toggle("hidden");
+}
+
+handleAddFood = (food) => {
+  this.setState({
+    foods: [food, ...this.state.foods],
+  });
+
+  this.handleForm();
+};
+
+handleSearchValue = (searchValue) => {
+  this.setState({
+    searchValue: searchValue,
+  });
+};
+
+
+render() {
+  const filteredFoods = this.state.foods.filter((food) => {
+    return food.name
+      .toLowerCase()
+      .includes(this.state.searchValue.toLowerCase());
+  });
+
+return (
+  <div className="App">
+
+      <Search
+        value={this.state.searchValue}
+        callbackFn={this.handleSearchValue}
+      />
+
+    <div>
+      <button onClick={this.handleForm}>Add New Food</button>
+      <div id="foodForm" className="hidden" >
+        <FoodForm callbackFn={this.handleAddFood}/>
+      </div>
     </div>
+
+    <div className="flex">
+      <div>
+        {filteredFoods.map((food) => {
+          return (
+            <FoodBox
+            name={food.name}
+            calories={food.calories}
+            image={food.image}
+            quantity={food.quantity}
+            />
+          );
+        })}
+      </div>
+      <div>
+        <h2>Today's Foods:</h2>
+        <p>Total: 0 cal</p>
+      </div>
+    </div>
+
+    
+  </div>
   );
 }
+}
+
 
 export default App;
